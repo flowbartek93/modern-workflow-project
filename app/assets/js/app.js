ac08bef487711b9fda4e7da1736d4bdf36f78e48
Bartek <submission93@gmail.com>
2021-01-04 16:00:15 +0100

    project finished

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -3,7 +3,7 @@ import _ from 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-
+import ClientArea from './modules/ClientArea'
 
 
 
@@ -13,7 +13,7 @@ let sticky = new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
-
+new ClientArea();
 
 
 let modal;
