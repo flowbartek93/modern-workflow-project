42a15d547247e1157fa0a7dcb69ae2fcf963ed1d
Bartek <submission93@gmail.com>
2021-01-04 15:36:34 +0100

    lambda f created

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,7 +1,27 @@
 exports.handler = function (event, context, callback) {
 
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to secret area"
-    })
+    const secretContent = `
+    <h3>Welcome to The secret Area</h3>
+    `
+    let body;
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401,
+
+        })
+    }
+
+
 }
